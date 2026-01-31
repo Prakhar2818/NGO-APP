5b1f6b6dc7cbafe5ec41745141c530c2fcb92781
Prakhar2818 <prakharattarde95@gmail.com>
2026-01-31 17:22:15 +0530

    cors steup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,21 @@
 // App File
 
-import express, {Application} from "express";
+import express, { Application } from "express";
 import cors from "cors";
 import authRoutes from "./modules/auth/routes/auth.routes.js";
 import profileRoutes from "./modules/auth/routes/profile.routes.js";
-import donationRoutes from "./modules/donation/routes/donation.routes.js"
+import donationRoutes from "./modules/donation/routes/donation.routes.js";
 
 const app: Application = express();
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://ngo-app-theta.vercel.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
